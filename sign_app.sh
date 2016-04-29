5bc20dc8af7743f1ebf19bc476f5a872a736f12d
Liam Sharp <liam.sharp@gmail.com>
2016-04-29 14:32:29 +0100

    Put shell script app in its own dir

--- sign_app.sh
+++ sign_app.sh
@@ -10,7 +10,7 @@ echo "Signing Identify set as: '$signing_identity'"
 
 rm -Rf target/
 mkdir target
-cp -R HelloWorld.app target/
+cp -R apps target/
 pushd target/
 find . -name .DS_Store -delete
 find . -name *~ -delete
@@ -18,7 +18,7 @@ popd
 
 echo "Signing with developer id using provided Signing Identity"
 export CODESIGN_ALLOCATE="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/codesign_allocate" 
-codesign -s "$signing_identity" -v target/HelloWorld.app/
+codesign -s "$signing_identity" -v target/apps/shell-script/HelloWorld.app/
 OUT=$?
 
 if [ $OUT -eq 0 ];then
@@ -27,3 +27,4 @@ else
     echo "developer id signing failed, failing build."
     exit $OUT
 fi
+
